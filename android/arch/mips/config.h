b86ab62a28a084147b1026e03c24761350438fbb
Yegor Yelizarov <yyelizarov@embeddedalley.com>
2009-11-10 08:43:28 -0500

    Missing config.h files added
    Signed-off-by: Yegor Yelizarov <yyelizarov@embeddedalley.com>

---
+++ android/arch/mips/config.h
@@ -0,0 +1,341 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
+#define ENABLE_NLS 1
+
+/* Define if AAC is using new api prefix */
+#define FAAD_IS_NEAAC 1
+
+/* gettext package name */
+#define GETTEXT_PACKAGE "gst-plugins-bad-0.10"
+
+/* Define if GSM header in gsm/ subdir */
+/* #undef GSM_HEADER_IN_SUBDIR */
+
+/* Defined if gcov is enabled to force a rebuild due to config.h changing */
+/* #undef GST_GCOV_ENABLED */
+
+/* Default errorlevel to use */
+#define GST_LEVEL_DEFAULT GST_LEVEL_NONE
+
+/* GStreamer license */
+#define GST_LICENSE "LGPL"
+
+/* mjpegtools API evolution */
+#define GST_MJPEGTOOLS_API 0
+
+/* package name in plugins */
+#define GST_PACKAGE_NAME "GStreamer Bad Plug-ins source release"
+
+/* package origin */
+#define GST_PACKAGE_ORIGIN "Unknown package origin"
+
+/* Define to enable alsa plug-ins (used by gstalsa). */
+#define HAVE_ALSA 
+
+/* Define to enable amrwb library (used by amrwb). */
+/* #undef HAVE_AMRWB */
+
+/* Define to enable bz2 library (used by bz2). */
+#define HAVE_BZ2 
+
+/* Define to enable cdaudio (used by cdaudio). */
+/* #undef HAVE_CDAUDIO */
+
+/* Define if the host CPU is an Alpha */
+/* #undef HAVE_CPU_ALPHA */
+
+/* Define if the host CPU is an ARM */
+/* #undef HAVE_CPU_ARM */
+
+/* Define if the host CPU is a CRIS */
+/* #undef HAVE_CPU_CRIS */
+
+/* Define if the host CPU is a CRISv32 */
+/* #undef HAVE_CPU_CRISV32 */
+
+/* Define if the host CPU is a HPPA */
+/* #undef HAVE_CPU_HPPA */
+
+/* Define if the host CPU is an x86 */
+/* #undef HAVE_CPU_I386 */
+
+/* Define if the host CPU is a IA64 */
+/* #undef HAVE_CPU_IA64 */
+
+/* Define if the host CPU is a M68K */
+/* #undef HAVE_CPU_M68K */
+
+/* Define if the host CPU is a MIPS */
+#define HAVE_CPU_MIPS 1
+
+/* Define if the host CPU is a PowerPC */
+/* #undef HAVE_CPU_PPC */
+
+/* Define if the host CPU is a 64 bit PowerPC */
+/* #undef HAVE_CPU_PPC64 */
+
+/* Define if the host CPU is a S390 */
+/* #undef HAVE_CPU_S390 */
+
+/* Define if the host CPU is a SPARC */
+/* #undef HAVE_CPU_SPARC */
+
+/* Define if the host CPU is a x86_64 */
+/* #undef HAVE_CPU_X86_64 */
+
+/* Define to enable libdc1394 (used by dc1394). */
+/* #undef HAVE_DC1394 */
+
+/* Define if the GNU dcgettext() function is already present or preinstalled.
+   */
+#define HAVE_DCGETTEXT 1
+
+/* Define to enable dirac (used by dirac). */
+/* #undef HAVE_DIRAC */
+
+/* Define to enable directfb (used by dfbvideosink ). */
+/* #undef HAVE_DIRECTFB */
+
+/* Define to enable divx plugins (used by divx). */
+/* #undef HAVE_DIVX */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* define for working do while(0) macros */
+#define HAVE_DOWHILE_MACROS 1
+
+/* Define to enable dts library (used by dtsdec). */
+/* #undef HAVE_DTS */
+
+/* Define to enable DVB Source (used by dvb). */
+#define HAVE_DVB 
+
+/* Define to enable building of experimental plug-ins. */
+/* #undef HAVE_EXPERIMENTAL */
+
+/* Define to enable building of plug-ins with external deps. */
+#define HAVE_EXTERNAL 
+
+/* Define to enable AAC encoder plug-in (used by faac). */
+#define HAVE_FAAC 
+
+/* Define to enable AAC decoder plug-in (used by faad). */
+#define HAVE_FAAD 
+
+/* Define to enable linux framebuffer (used by fbdevsink). */
+#define HAVE_FBDEV 
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define to enable GSM library (used by gsmenc gsmdec). */
+/* #undef HAVE_GSM */
+
+/* Define if you have the iconv() function. */
+/* #undef HAVE_ICONV */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to enable integer vorbis plug-in (used by ivorbisdec). */
+/* #undef HAVE_IVORBIS */
+
+/* Define to enable Jack (used by jack). */
+/* #undef HAVE_JACK */
+
+/* Define to enable ladspa (used by ladspa). */
+/* #undef HAVE_LADSPA */
+
+/* Define to 1 if you have the `asound' library (-lasound). */
+/* #undef HAVE_LIBASOUND */
+
+/* Define to 1 if you have the `divxdecore' library (-ldivxdecore). */
+/* #undef HAVE_LIBDIVXDECORE */
+
+/* Define to 1 if you have the `divxencore' library (-ldivxencore). */
+/* #undef HAVE_LIBDIVXENCORE */
+
+/* Define to enable mms protocol library (used by libmms). */
+/* #undef HAVE_LIBMMS */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to enable METADATA muxer and demuxer (used by metadata). */
+#define HAVE_METADATA 
+
+/* Define to enable mpeg2enc (used by mpeg2enc). */
+/* #undef HAVE_MPEG2ENC */
+
+/* Define to enable mplex (used by mplex). */
+/* #undef HAVE_MPLEX */
+
+/* Define to enable musepackdec (used by musepack). */
+/* #undef HAVE_MUSEPACK */
+
+/* Define to enable musicbrainz tag generation (used by musicbrainz). */
+/* #undef HAVE_MUSICBRAINZ */
+
+/* Define to enable MythTV client plugins (used by mythtvsrc). */
+/* #undef HAVE_MYTHTV */
+
+/* Define to enable nas plug-in (used by nassink). */
+/* #undef HAVE_NAS */
+
+/* Define to enable neon http client plugins (used by neonhttpsrc). */
+/* #undef HAVE_NEON */
+
+/* Define to enable ofa plugins (used by ofa). */
+/* #undef HAVE_OFA */
+
+/* Define to enable Open Sound System 4 (used by oss4). */
+#define HAVE_OSS4 
+
+/* Define to enable QuickTime wrapper (used by qtwrapper). */
+/* #undef HAVE_QUICKTIME */
+
+/* Define if RDTSC is available */
+#define HAVE_RDTSC 1
+
+/* Define to enable SDL plug-in (used by sdlvideosink sdlaudiosink). */
+#define HAVE_SDL 
+
+/* Define to enable sndfile plug-in (used by sfsrc sfsink). */
+/* #undef HAVE_SNDFILE */
+
+/* Define to enable soundtouch plug-in (used by soundtouch). */
+/* #undef HAVE_SOUNDTOUCH */
+
+/* Define to enable spc decoder (used by spc). */
+/* #undef HAVE_SPC */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to enable swfdec plug-in (used by swfdec). */
+/* #undef HAVE_SWFDEC */
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to enable ogg theora codec (used by theoraexpdec). */
+/* #undef HAVE_THEORADEC */
+
+/* Define to enable timidity midi soft synth plugin (used by timidity). */
+/* #undef HAVE_TIMIDITY */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define if valgrind should be used */
+/* #undef HAVE_VALGRIND */
+
+/* Define to enable Video CD (used by vcdsrc). */
+#define HAVE_VCD 
+
+/* Define to enable wildmidi midi soft synth plugin (used by wildmidi). */
+/* #undef HAVE_WILDMIDI */
+
+/* Define to 1 if you have the <windows.h> header file. */
+/* #undef HAVE_WINDOWS_H */
+
+/* Define to enable Windows internet library (used by wininet). */
+/* #undef HAVE_WININET */
+
+/* Define to 1 if you have the <wininet.h> header file. */
+/* #undef HAVE_WININET_H */
+
+/* Define to 1 if you have the <winsock2.h> header file. */
+/* #undef HAVE_WINSOCK2_H */
+
+/* Define to enable x264 plug-in (used by x264). */
+/* #undef HAVE_X264 */
+
+/* Define to enable xvid plugins (used by xvid). */
+/* #undef HAVE_XVID */
+
+/* the host CPU */
+#define HOST_CPU "MIPS"
+
+/* gettext locale dir */
+#define LOCALEDIR "/usr/local/share/locale"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "gst-plugins-bad"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://bugzilla.gnome.org/enter_bug.cgi?product=GStreamer"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GStreamer Bad Plug-ins"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GStreamer Bad Plug-ins 0.10.7"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gst-plugins-bad"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.10.7"
+
+/* directory where plugins are located */
+#define PLUGINDIR "/usr/local/lib/gstreamer-0.10"
+
+/* The size of `char', as computed by sizeof. */
+/* #undef SIZEOF_CHAR */
+
+/* The size of `int', as computed by sizeof. */
+/* #undef SIZEOF_INT */
+
+/* The size of `long', as computed by sizeof. */
+/* #undef SIZEOF_LONG */
+
+/* The size of `short', as computed by sizeof. */
+/* #undef SIZEOF_SHORT */
+
+/* The size of `void*', as computed by sizeof. */
+/* #undef SIZEOF_VOIDP */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define location of timidity.cfg */
+/* #undef TIMIDITY_CFG */
+
+/* Version number of package */
+#define VERSION "0.10.7"
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
